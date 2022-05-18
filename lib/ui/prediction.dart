9cc27d11a8b417f4215d041199d9fb7f9f5ae7c7
Daniel Fernández <47828494+7yrionLannister@users.noreply.github.com>
2022-05-18 15:52:01 -0500

    You can select the date and have the data plotted for that period

--- lib/ui/prediction.dart
+++ lib/ui/prediction.dart
@@ -4,7 +4,6 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../model/cafeteria.dart';
 
-
 class Prediction extends StatefulWidget {
   final List<Cafeteria> cafeterias;
   const Prediction(this.cafeterias, {Key? key}) : super(key: key);
@@ -28,156 +27,172 @@ class _PredictionState extends State<Prediction> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffffffff),
-      appBar: AppBar(
-        title: const Text(
-          'Predicción de aforo',
-          style: TextStyle(fontFamily: 'Montserrat', fontSize: 22.0),
+        backgroundColor: const Color(0xffffffff),
+        appBar: AppBar(
+          title: const Text(
+            'Predicción de aforo',
+            style: TextStyle(fontFamily: 'Montserrat', fontSize: 22.0),
+          ),
         ),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(10),
-            decoration: const BoxDecoration(
-              color: Color(0xffffffff),
-            ),
-            width: MediaQuery.of(context).size.width,
-            //height: MediaQuery.of(context).size.height,
-              height: 700,
-            child: Column(
-              children: [
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: const <Widget>[
-                          Text("Cafetería",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat',
-                                  fontSize: 22.0)),
-                        ],
-                      ),
-                      const Spacer(),
-                      Column(
-                        children: <Widget>[
-                          SizedBox(
-                            child: DropdownButton<Cafeteria>(
-                              value: selectedCafeteria,
-                              items: cafeterias.map((Cafeteria cafeteria) {
-                                return DropdownMenuItem(
-                                  value: cafeteria,
-                                  child: Text(cafeteria.name),
-                                );
-                              }).toList(),
-                              onChanged: (Cafeteria? value) {
-                                setState(() {
-                                  selectedCafeteria = value!;
-                                });
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ]),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+        body: ListView(
+          children: [
+            Container(
+                margin: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  color: Color(0xffffffff),
+                ),
+                width: MediaQuery.of(context).size.width,
+                //height: MediaQuery.of(context).size.height,
+                height: 700,
+                child: Column(
                   children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const <Widget>[
-                        Text("Fecha",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Montserrat',
-                                fontSize: 22.0)),
-                      ],
-                    ),
-                    const Spacer(),
-                    Column(
-                      children: <Widget>[
-                        SizedBox(
-                          child:
-                          TextButton(child: const
-                          Text("Elegir",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontFamily: 'Montserrat',
-                                  fontSize: 16.0)
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const <Widget>[
+                              Text("Cafetería",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 22.0)),
+                            ],
                           ),
-                            onPressed: () {
-                              showDatePicker(
-                                  builder: (context, child) {
-                                    return Theme(
-                                      data: Theme.of(context).copyWith(
-                                        colorScheme: const ColorScheme.light(
-                                          primary: Color(0xff1c162e), // header background color
-                                          onPrimary: Colors.white, // header text color
-                                          onSurface: Colors.black, // body text color
-                                        ),
-                                        textButtonTheme: TextButtonThemeData(
-                                          style: TextButton.styleFrom(
-                                            primary: const Color(0xff1c162e), // button text color
+                          const Spacer(),
+                          Column(
+                            children: <Widget>[
+                              SizedBox(
+                                child: DropdownButton<Cafeteria>(
+                                  value: selectedCafeteria,
+                                  items: cafeterias.map((Cafeteria cafeteria) {
+                                    return DropdownMenuItem(
+                                      value: cafeteria,
+                                      child: Text(cafeteria.name),
+                                    );
+                                  }).toList(),
+                                  onChanged: (Cafeteria? value) {
+                                    setState(() {
+                                      selectedCafeteria = value!;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const <Widget>[
+                            Text("Fecha",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 22.0)),
+                          ],
+                        ),
+                        const Spacer(),
+                        Column(
+                          children: <Widget>[
+                            SizedBox(
+                              child: TextButton(
+                                child: Text(dateForPrediction.toString().substring(0, 10),
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontFamily: 'Montserrat',
+                                        fontSize: 16.0)),
+                                onPressed: () {
+                                  showDatePicker(
+                                    builder: (context, child) {
+                                      return Theme(
+                                        data: Theme.of(context).copyWith(
+                                          colorScheme: const ColorScheme.light(
+                                            primary: Color(
+                                                0xff1c162e), // header background color
+                                            onPrimary: Colors
+                                                .white, // header text color
+                                            onSurface:
+                                                Colors.black, // body text color
+                                          ),
+                                          textButtonTheme: TextButtonThemeData(
+                                            style: TextButton.styleFrom(
+                                              primary: const Color(
+                                                  0xff1c162e), // button text color
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                      child: child!,
-                                    );
-                                  },
-                                  context: context,
-                                  initialDate: DateTime.now(),
-                                  firstDate: DateTime.now(),
-                                  lastDate: DateTime.now().add(const Duration(days: 7)),
-                              );
-                            },
-                          ),
+                                        child: child!,
+                                      );
+                                    },
+                                    context: context,
+                                    initialDate: dateForPrediction,
+                                    firstDate: DateTime.now(),
+                                    lastDate: DateTime.now()
+                                        .add(const Duration(days: 7)),
+                                  ).then((value) => {
+                                        setState(() {
+                                          dateForPrediction = value!;
+                                        })
+                                      });
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
+                    const Spacer(),
+                    _buildSplineChart(
+                        "Ocupación de mesas",
+                        selectedCafeteria.tablesOccupationHistory,
+                        Colors.pinkAccent),
+                    _buildSplineChart(
+                        "Ocupación de sillas",
+                        selectedCafeteria.peopleOccupationHistory,
+                        Colors.deepPurpleAccent),
                   ],
-                ),
-                const Spacer(),
-                _buildSplineChart("Ocupación de mesas", selectedCafeteria.tablesOccupationHistory, Colors.pinkAccent),
-                _buildSplineChart("Ocupación de sillas", selectedCafeteria.peopleOccupationHistory, Colors.deepPurpleAccent),
-              ],
-            )
-        ),
-        ],
-      )
-    );
+                )),
+          ],
+        ));
   }
 
-  Widget _buildSplineChart(String title, List<OccupationTimestamp> completeData, Color color) {
+  Widget _buildSplineChart(
+      String title, List<OccupationTimestamp> completeData, Color color) {
     var data = _filterDataForOneDay(completeData);
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Column(
           children: <Widget>[
-            Text(title,
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Montserrat',
-                    fontSize: 18.0)
+            Row(
+              children: [
+                CircleAvatar(backgroundColor: color, radius: 5,),
+                Text(" " + title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        fontSize: 18.0)),
+              ],
             ),
             SizedBox(
               child: SfCartesianChart(
-                primaryXAxis: CategoryAxis(),
+                  primaryXAxis: CategoryAxis(),
                   series: <ChartSeries>[
                     // Renders spline chart
                     SplineSeries<OccupationTimestamp, String>(
                       dataSource: data,
-                      xValueMapper: (OccupationTimestamp ot, _) => _dateTimeToHourAndMinuteString(ot.time, _.isEven),
-                      yValueMapper: (OccupationTimestamp ot, _) => ot.occupation,
+                      xValueMapper: (OccupationTimestamp ot, _) =>
+                          _dateTimeToHourAndMinuteString(ot.time, _.isEven),
+                      yValueMapper: (OccupationTimestamp ot, _) =>
+                          ot.occupation,
                       color: color,
                     )
-                  ]
-              ),
-              width: 300,
+                  ]),
+              width: 360,
               height: 250,
             ),
           ],
@@ -186,10 +201,11 @@ class _PredictionState extends State<Prediction> {
     );
   }
 
-  List<OccupationTimestamp> _filterDataForOneDay(List<OccupationTimestamp> completeData) {
+  List<OccupationTimestamp> _filterDataForOneDay(
+      List<OccupationTimestamp> completeData) {
     List<OccupationTimestamp> filtered = [];
-    for(OccupationTimestamp ot in completeData) {
-      if(ot.time.day == dateForPrediction.day) {
+    for (OccupationTimestamp ot in completeData) {
+      if (ot.time.day == dateForPrediction.day) {
         filtered.add(ot);
       }
     }
@@ -198,11 +214,11 @@ class _PredictionState extends State<Prediction> {
 
   String _dateTimeToHourAndMinuteString(DateTime dateTime, bool isEven) {
     String hour = dateTime.hour.toString();
-    if(hour.length < 2) {
+    if (hour.length < 2) {
       hour = '0' + hour;
     }
     String minute = dateTime.minute.toString();
-    if(minute.length < 2) {
+    if (minute.length < 2) {
       minute = '0' + minute;
     }
     return "$hour:$minute";
